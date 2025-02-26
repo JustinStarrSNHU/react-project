c16a2ac0906bea96d54fdfb7ded21104b2968c4e
JustinStarrSNHU <justin.starr@snhu.edu>
2025-02-26 13:45:55 -0600

    Working with the useEffect hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useReducer} from "react";
+import { useEffect, useReducer} from "react";
 import './App.css'
 import chef from "./images/chef.jpg";
 
@@ -52,6 +52,10 @@ function App() {
     (status) => !status,
     true
   );
+
+  useEffect(() => {
+    console.log(`The restaurant is ${status ? "open" : "closed"}`);
+  }, [status]);
 
   return (
     <div>
